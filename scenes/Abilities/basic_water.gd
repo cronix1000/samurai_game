fda2c0d2f6aac87f1e66edf7b77339c4133d7c22
Bram <145325665+Bram-VE@users.noreply.github.com>
2024-01-30 17:25:22 +0100

    Fixed projectiles

--- scenes/Abilities/basic_water.gd
+++ scenes/Abilities/basic_water.gd
@@ -6,19 +6,24 @@ var aim_pos
 @onready var attack_sound = $attack_sound
 
 var speed = 200
-var controller = null
+var setDirection : bool
 
 func _ready():
 	player.play("flying")
 	attack_sound.play()
-	if(velocity.x) < 0:
-		sprite.flip_h = true
 	await get_tree().create_timer(2).timeout
 	queue_free()
 	#Explote etc...
 
 func _physics_process(delta):
 	if aim_pos:
+		if !setDirection:
+			if aim_pos.x > global_position.x:
+				sprite.flip_h = false
+			else:
+				sprite.flip_h = true
+				speed = - speed
+			setDirection = true
 		global_position.y = aim_pos.y
 		global_position.x += speed * delta
 
